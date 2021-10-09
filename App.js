300cf3e4c18750379ed7a7a053b87920884a5333
imlai77 <imlai77@gmail.com>
2021-10-09 17:08:00 -0400

    added button

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, SafeAreaView, Alert, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
       <Text>Don't Fall on Da Wei?</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Press me"
+        color="#f194ff"
+        onPress={() => Alert.alert('Button with adjusted color pressed')}
+      />
     </View>
   );
 }
